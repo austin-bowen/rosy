0e9e46df8fee1827e74f64ab38d3d1d94be510a2
Austin Bowen <austin.bowen.314@gmail.com>
2024-10-18 18:07:39 -0400

    Add defaults to build_mesh_coordinator_server

--- src/easymesh/coordinator/server.py
+++ src/easymesh/coordinator/server.py
@@ -4,6 +4,7 @@ from asyncio import StreamReader
 from codecs import StreamWriter
 from typing import Optional
 
+from easymesh.coordinator.constants import DEFAULT_COORDINATOR_PORT
 from easymesh.objectstreamio import CodecObjectStreamIO
 from easymesh.reqres import MeshTopologyBroadcast, RegisterNodeRequest, RegisterNodeResponse
 from easymesh.rpc import ObjectStreamRPC, RPC
@@ -95,8 +96,8 @@ class RPCMeshCoordinatorServer(MeshCoordinatorServer):
 
 
 def build_mesh_coordinator_server(
-        host: str,
-        port: int,
+        host: str = '',
+        port: int = DEFAULT_COORDINATOR_PORT,
 ) -> MeshCoordinatorServer:
     async def start_stream_server(cb):
         return await asyncio.start_server(cb, host=host, port=port)
