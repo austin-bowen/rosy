522cc2eace7e04ef55fb2b36640e158d885ce6aa
Austin Bowen <austin.bowen.314@gmail.com>
2024-10-19 23:04:07 -0400

    Add --name and --coordinator-host args to sender and receiver demo nodes

--- src/easymesh/demo/receiver_node.py
+++ src/easymesh/demo/receiver_node.py
@@ -1,3 +1,4 @@
+import argparse
 import asyncio
 
 import easymesh
@@ -10,10 +11,22 @@ async def handle_message(topic: Topic, data: Data):
 
 
 async def main():
-    node = await easymesh.build_mesh_node(name='receiver')
+    args = _parse_args()
+
+    node = await easymesh.build_mesh_node(
+        name=args.name,
+        coordinator_host=args.coordinator_host,
+    )
 
     await node.add_listener('some-topic', handle_message)
     await forever()
+
+
+def _parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--name', default='receiver')
+    parser.add_argument('--coordinator-host', default='localhost')
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
--- src/easymesh/demo/sender_node.py
+++ src/easymesh/demo/sender_node.py
@@ -1,14 +1,27 @@
+import argparse
 import asyncio
 
 import easymesh
 
 
 async def main():
-    node = await easymesh.build_mesh_node(name='sender')
+    args = _parse_args()
+
+    node = await easymesh.build_mesh_node(
+        name=args.name,
+        coordinator_host=args.coordinator_host,
+    )
 
     while True:
         await node.send('some-topic', f'Hello from node {node}!')
         await asyncio.sleep(1)
+
+
+def _parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--name', default='sender')
+    parser.add_argument('--coordinator-host', default='localhost')
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
