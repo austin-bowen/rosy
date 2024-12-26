9e2501763e87db2b6381ae890da81a9f5d0c8955
Austin Bowen <austin.bowen.314@gmail.com>
2024-12-26 00:26:34 -0500

    Revamp sender to allow sending arbitrary messages

--- src/easymesh/demo/sender.py
+++ src/easymesh/demo/sender.py
@@ -1,15 +1,68 @@
 import asyncio
+from argparse import Namespace
+from datetime import datetime
 
 from easymesh import build_mesh_node_from_args
+from easymesh.argparse import get_node_arg_parser
 
 
-async def main():
-    node = await build_mesh_node_from_args(default_node_name='sender')
+async def main(args: Namespace):
+    node = await build_mesh_node_from_args(args=args)
+
+    def log(msg_) -> None:
+        now = datetime.now()
+        print(f'[{now}] [{node.id.name}] {msg_}')
+
+    topic_sender = node.get_topic_sender(args.topic)
+
+    data = eval(args.data) if args.eval else args.data
 
     while True:
-        await node.send('some-topic', {'hello': 'world!'})
-        await asyncio.sleep(1)
+        log(f'Sending topic={args.topic!r} data={args.data!r}')
+
+        if not await topic_sender.has_listeners():
+            log(f'WARNING: No listeners on topic {args.topic!r}.')
+
+        await topic_sender.send(data)
+
+        if args.interval < 0:
+            return
+        await asyncio.sleep(args.interval)
+
+
+def parse_args() -> Namespace:
+    parser = get_node_arg_parser(default_node_name='sender')
+
+    parser.add_argument(
+        '--topic', '-t',
+        default='some-topic',
+        help='The topic to send to. Default: %(default)s',
+    )
+
+    parser.add_argument(
+        '--data', '-d',
+        default='Hello, world!',
+        help='The data to send. By default, data is interpreted as a string. '
+             'Use the `--eval` flag to interpret it as Python code. '
+             'Default: %(default)s',
+    )
+
+    parser.add_argument(
+        '--eval', '-e',
+        action='store_true',
+        help='Evaluate the data as Python code. Default: %(default)s',
+    )
+
+    parser.add_argument(
+        '--interval', '-i',
+        default=-1,
+        type=float,
+        help='The interval in seconds to send messages. A value < 0 will '
+             'cause the message to be sent only once. Default: %(default)s',
+    )
+
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    asyncio.run(main(parse_args()))
