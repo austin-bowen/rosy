52ec3372b1f3986f84b74e1545217c0197e89c82
Austin Bowen <austin.bowen.314@gmail.com>
2024-12-26 00:37:22 -0500

    Revamp receiver to allow receiving on multiple topics

--- src/easymesh/demo/receiver.py
+++ src/easymesh/demo/receiver.py
@@ -1,18 +1,41 @@
 import asyncio
+from argparse import Namespace
+from datetime import datetime
 
 from easymesh import build_mesh_node_from_args
+from easymesh.argparse import get_node_arg_parser
 from easymesh.asyncio import forever
 
 
-async def handle_message(topic, data):
-    print(f'receiver got: topic={topic!r} data={data!r}')
+async def main(args: Namespace):
+    node = await build_mesh_node_from_args(args=args)
 
+    def log(msg_) -> None:
+        now = datetime.now()
+        print(f'[{now}] [{node.id.name}] {msg_}')
 
-async def main():
-    node = await build_mesh_node_from_args(default_node_name='receiver')
-    await node.listen('some-topic', handle_message)
+    async def handle_message(topic, data):
+        log(f'Received topic={topic!r} data={data!r}')
+
+    for topic in args.topics:
+        await node.listen(topic, handle_message)
+
+    log(f'Listening to topics: {args.topics}')
     await forever()
 
 
+def parse_args() -> Namespace:
+    parser = get_node_arg_parser(default_node_name='receiver')
+
+    parser.add_argument(
+        '--topics', '-t',
+        nargs='+',
+        default=['some-topic'],
+        help='The topics to listen to. Default: %(default)s',
+    )
+
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    asyncio.run(main())
+    asyncio.run(main(parse_args()))
