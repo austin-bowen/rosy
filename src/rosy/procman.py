064e6ea318832f23e4a5927bff33050971e21eaa
Austin Bowen <austin.bowen.314@gmail.com>
2025-08-05 01:20:23 -0400

    Simplify procman.py

--- src/rosy/procman.py
+++ src/rosy/procman.py
@@ -1,5 +1,4 @@
 import signal
-from dataclasses import dataclass
 from subprocess import Popen, TimeoutExpired
 from typing import Any
 
@@ -8,12 +7,14 @@ class ProcessManager:
     processes: list[Popen]
     """Processes managed by this manager."""
 
+    options: dict[str, Any]
+    """Default options for Popen."""
+
     timeout: float | None
-    """Default timeout for stopping processes."""
+    """Default timeout when stopping processes."""
 
     def __init__(
             self,
-            python_exe: str = 'python',
             options: dict[str, Any] = None,
             timeout: float | None = 10.,
     ):
@@ -24,61 +25,27 @@ class ProcessManager:
         Example:
 
             with ProcessManager() as pm:
-                # Run: ls -l
-                pm.popen(['ls', '-l'])
-
-                pm.start_python('my_script.py', arg1='value1')
-                pm.start_python('my_other_script.py', arg2='value2')
+                pm.popen('ls -l', shell=True)
+                pm.popen(['rosy', 'coordinator'])
+                pm.popen(['python', 'script.py'])
 
         Args:
-            python_exe:
-                Path to the Python executable to use for starting Python processes.
             options:
                 Default kwargs to pass to Popen.
             timeout:
-                Default timeout for stopping processes.
+                Default timeout when stopping processes.
         """
 
-        self.python_exe = python_exe
+        self.options = options or {}
         self.timeout = timeout
 
-        self.processes: list[Popen] = []
-        self._options: dict[str, Any] = options or {}
-
-    @property
-    def options(self) -> dict[str, Any]:
-        return dict(self._options)
+        self.processes = []
 
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.stop()
-
-    def using_options(self, **options) -> '_OptionsContext':
-        """
-        Use with a ``with`` statement to temporarily use different default
-        options for Popen. Useful when you want to run a bunch of processes
-        with a different set of options.
-
-        Example:
-
-            with ProcessManager(shell=False) as pm:
-                pm.popen(['ls', '-l'])
-
-                with pm.using_options(shell=True):
-                    pm.popen('ls -l')
-                    pm.popen('cp foo bar')
-                    pm.popen('cat bar')
-
-        Args:
-            **options:
-                New default options to use for Popen.
-
-        Returns: A context manager.
-        """
-
-        return _OptionsContext(self, options)
 
     def popen(self, args, **kwargs) -> Popen:
         """
@@ -91,31 +58,13 @@ class ProcessManager:
         Returns: The Popen object for the new process.
         """
 
-        kwargs = {**self._options, **kwargs}
+        kwargs = {**self.options, **kwargs}
         return self.add(Popen(args, **kwargs))
 
     def add(self, process: Popen) -> Popen:
         """Add an existing process to the manager."""
         self.processes.append(process)
         return process
-
-    def start_python(self, args, **kwargs) -> Popen:
-        """
-        Start a new Python process.
-
-        Args:
-            args: Command arguments appended to the Python exe and passed to Popen.
-            **kwargs: Keyword arguments passed to Popen.
-
-        Returns: The Popen object for the new process.
-        """
-
-        if isinstance(args, str):
-            args = f'"{self.python_exe}" {args}'
-        else:
-            args = [self.python_exe] + list(args)
-
-        return self.popen(args, **kwargs)
 
     def stop(
             self,
@@ -179,18 +128,3 @@ class ProcessManager:
         for process in list(self.processes):
             process.wait(timeout)
             self.processes.remove(process)
-
-
-@dataclass
-class _OptionsContext:
-    procman: ProcessManager
-    options: dict[str, Any]
-    prev_options: dict[str, Any] = None
-
-    def __enter__(self):
-        self.prev_options = self.procman._options
-        self.procman._options = self.options
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.procman._options = self.prev_options
