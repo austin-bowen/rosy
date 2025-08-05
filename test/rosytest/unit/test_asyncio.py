adfec96919dcaeede741542702fb60acc2f31cc5
Austin Bowen <austin.bowen.314@gmail.com>
2025-08-05 00:44:53 -0400

    Add TestLockableWriter

--- test/rosytest/unit/test_asyncio.py
+++ test/rosytest/unit/test_asyncio.py
@@ -2,7 +2,7 @@ from unittest.mock import create_autospec
 
 import pytest
 
-from rosy.asyncio import Writer, close_ignoring_errors, noop
+from rosy.asyncio import LockableWriter, Writer, close_ignoring_errors, noop
 
 
 class TestCloseIgnoringErrors:
@@ -36,3 +36,53 @@ class TestCloseIgnoringErrors:
 @pytest.mark.asyncio
 async def test_noop():
     assert await noop() is None
+
+
+class TestLockableWriter:
+    def setup_method(self):
+        self.writer = create_autospec(Writer)
+        self.lockable_writer = LockableWriter(self.writer)
+
+    def test_constructor(self):
+        assert self.lockable_writer.writer is self.writer
+
+    @pytest.mark.asyncio
+    async def test_write_succeeds_when_locked(self):
+        async with self.lockable_writer:
+            self.lockable_writer.write(b'test')
+
+        self.writer.write.assert_called_once_with(b'test')
+
+    @pytest.mark.asyncio
+    async def test_write_fails_when_not_locked(self):
+        with pytest.raises(
+                RuntimeError,
+                match="Writer must be locked before writing",
+        ):
+            self.lockable_writer.write(b'test')
+
+        self.writer.write.assert_not_called()
+
+    @pytest.mark.asyncio
+    async def test_drain(self):
+        await self.lockable_writer.drain()
+        self.writer.drain.assert_awaited_once()
+
+    def test_close(self):
+        self.lockable_writer.close()
+        self.writer.close.assert_called_once()
+
+    def test_is_closing(self):
+        self.writer.is_closing.return_value = True
+        assert self.lockable_writer.is_closing() is True
+        self.writer.is_closing.assert_called_once()
+
+    @pytest.mark.asyncio
+    async def test_wait_closed(self):
+        await self.lockable_writer.wait_closed()
+        self.writer.wait_closed.assert_awaited_once()
+
+    def test_get_extra_info(self):
+        self.writer.get_extra_info.return_value = 'info'
+        assert self.lockable_writer.get_extra_info('name') == 'info'
+        self.writer.get_extra_info.assert_called_once_with('name', None)
